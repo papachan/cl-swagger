193cb39f1f6246a014a95f92e4e185de23b52b17
Dan Loaiza <papachan@gmail.com>
2016-05-15 21:49:36 -0500

    change name

--- t/swagger-test.lisp
+++ t/swagger-test.lisp
@@ -7,15 +7,15 @@
 (in-package #:swagger-test)
 
 
-(def-suite tests)
-(in-suite tests)
+(def-suite swagger)
+(in-suite swagger)
 
 (test parse-test
   "a simple test with a method from swagger package"
   (is (equal t (swagger::post-through-url "http://google.com/"))))
 
 (defun run-tests ()
-  (5am:run! 'tests))
+  (5am:run! 'swagger))
 
 
 (run-tests)
